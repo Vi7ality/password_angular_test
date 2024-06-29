dcfd7fa668f4637054bb593929287a81e7827302
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-06-30 00:41:14 +0300

    Revert "requirements section added"
    This reverts commit 37efd84a82f8226aaf073c4e8347c49673fe582e.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { PasswordStrengthComponent } from './password-strength/password-strength.component';
-import { RequirementsComponent } from './requirements/requirements.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, PasswordStrengthComponent, RequirementsComponent],
+  imports: [RouterModule, PasswordStrengthComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
