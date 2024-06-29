37efd84a82f8226aaf073c4e8347c49673fe582e
VitaliiR <104318952+Vi7ality@users.noreply.github.com>
2024-06-29 19:36:51 +0300

    requirements section added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { PasswordStrengthComponent } from './password-strength/password-strength.component';
+import { RequirementsComponent } from './requirements/requirements.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, PasswordStrengthComponent],
+  imports: [RouterModule, PasswordStrengthComponent, RequirementsComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
